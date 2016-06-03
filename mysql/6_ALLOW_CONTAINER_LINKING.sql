969ffcd37b808f3e115d694e3e2a276096de94a6
Peri Subrahmanya <peri.subrahmanya@gmail.com>
2016-06-03 09:59:30 -0400

    Update 6_ALLOW_CONTAINER_LINKING.sql

--- mysql/6_ALLOW_CONTAINER_LINKING.sql
+++ mysql/6_ALLOW_CONTAINER_LINKING.sql
@@ -1,4 +1,4 @@
 use mysql;
-GRANT ALL ON *.* to root@'localhost' IDENTIFIED BY 'root'; 
-GRANT ALL ON *.* to root@'127.0.0.1' IDENTIFIED BY 'root';
+GRANT ALL ON *.* to etl@'172.17.0.3' IDENTIFIED BY 'etl'; 
+GRANT ALL ON *.* to etl@'127.17.0.3' IDENTIFIED BY 'etl';
 FLUSH PRIVILEGES; 
