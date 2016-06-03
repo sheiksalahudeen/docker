e26e1d82c5caf59b4ef2ecb5a1d97cb763be511f
Peri Health <info@perihealth.com>
2016-06-03 07:34:58 -0400

    Updated linking sql content with root password.

--- mysql/6_ALLOW_CONTAINER_LINKING.sql
+++ mysql/6_ALLOW_CONTAINER_LINKING.sql
@@ -1,4 +1,4 @@
 use mysql;
 GRANT ALL ON *.* to root@'localhost' IDENTIFIED BY 'root'; 
-GRANT ALL ON *.* to root@'127.0.0.1' IDENTIFIED BY 'your-root-password';
+GRANT ALL ON *.* to root@'127.0.0.1' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES; 
