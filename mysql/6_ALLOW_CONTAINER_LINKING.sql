e4f7ffbbf418b9219bdfe5177d958b8599b3e367
Peri Subrahmanya <peri.subrahmanya@gmail.com>
2016-06-03 14:36:21 -0400

    Update 6_ALLOW_CONTAINER_LINKING.sql

--- mysql/6_ALLOW_CONTAINER_LINKING.sql
+++ mysql/6_ALLOW_CONTAINER_LINKING.sql
@@ -1,4 +1,4 @@
 use mysql;
 GRANT ALL ON *.* to etl@'172.17.0.3' IDENTIFIED BY 'etl'; 
-GRANT ALL ON *.* to etl@'172.17.0.3' IDENTIFIED BY 'etl';
+GRANT ALL ON *.* to recap@'172.17.0.1' IDENTIFIED BY 'recap';
 FLUSH PRIVILEGES; 
