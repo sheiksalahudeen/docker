3986249ecfee813c74a61308ac58e961cc3acd29
Peri Subrahmanya <peris@Peris-MacBook-Pro.local>
2016-07-12 09:32:13 -0400

    Updated docker file to execute MySQL

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
+	echo "Copying default my.cnf to /usr/share/ folder"
 	cp /etc/mysql/my.cnf /usr/share/mysql/my-default.cnf
-	
+		
+	echo "Initialize mysql db"
 	mysql_install_db
+		
+	echo "Run mysql"
+	/usr/bin/mysqld_safe & tail -f /dev/null
 
-	/usr/bin/mysqld_safe &
-	sleep 10s
-
-	echo "GRANT ALL ON *.* to etl@'%' IDENTIFIED BY 'etl', recap@'%' IDENTIFIED BY 'recap'; FLUSH PRIVILEGES;" | mysql
-	
-	sleep 10s
 fi
+	echo "DB already initialized; Starting MySQL"
+	/usr/bin/mysqld_safe & tail -f /dev/null
+
 
