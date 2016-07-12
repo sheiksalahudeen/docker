9b86069326e23d04faec56bc43e4af0fb1763d29
Peri Subrahmanya <peri.subrahmanya@gmail.com>
2016-07-12 16:01:51 -0400

    Update startup.sh

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -4,11 +4,31 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	echo "Copying default my.cnf to /usr/share/ folder"
 	cp /opt/git/mysql/my.cnf /usr/share/mysql/my-default.cnf
 	cp /opt/git/mysql/my.cnf /etc/mysql/my.cnf
+	cp /opt/git/mysql/my.cnf /etc/my.cnf
 		
 	echo "Initialize mysql db"
 	mysql_install_db
-	
-	service mysql start & tail /dev/null
+		
+	echo "Run mysql"
+	/usr/bin/mysqld_safe & sleep 10s
+
+	mysql -uroot < /opt/git/mysql/1_SCHEMA_TABLES_CREATION.sql
+    	mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
+    	mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
+    	mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
+    	mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
+    	mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
+
+else 
+	echo "Copying default my.cnf to /usr/share/ folder"
+	cp /opt/git/mysql/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/mysql/my.cnf /etc/mysql/my.cnf
+	cp /opt/git/mysql/my.cnf /usr/my.cnf
+
+
+	echo "DB already initialized; Starting MySQL"
+	/usr/bin/mysqld_safe & tail -f /dev/null
+
 fi
 
 
