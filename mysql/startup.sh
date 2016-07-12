d7991c436f9580ad059e5aaa42bdd239c7ca9f3c
Peri Subrahmanya <peri.subrahmanya@gmail.com>
2016-07-12 14:47:22 -0400

    Update startup.sh

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -4,6 +4,7 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	echo "Copying default my.cnf to /usr/share/ folder"
 	cp /opt/git/mysql/my.cnf /usr/share/mysql/my-default.cnf
 	cp /opt/git/mysql/my.cnf /etc/mysql/my.cnf
+	cp /opt/git/mysql/my.cnf /usr/my.cnf
 		
 	echo "Initialize mysql db"
 	mysql_install_db
@@ -12,11 +13,11 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	/usr/bin/mysqld_safe & sleep 10s
 
 	mysql -uroot < /opt/git/mysql/1_SCHEMA_TABLES_CREATION.sql
-    mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
-    mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
-    mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
-    mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
-    mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
+    	mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
+    	mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
+    	mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
+    	mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
+    	mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
 
 
 
