28b437bc722c8f0a3e2aa9124e4aa8297d8bbfb5
Peri Subrahmanya <peri.subrahmanya@gmail.com>
2016-07-12 11:54:50 -0400

    Update startup.sh

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -3,6 +3,7 @@
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	echo "Copying default my.cnf to /usr/share/ folder"
 	cp /opt/git/mysql/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/mysql/my.cnf /etc/mysql/my.cnf
 		
 	echo "Initialize mysql db"
 	mysql_install_db
