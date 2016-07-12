2c7940eab25cf8f9922c98a023d87574f6738149
peris <peri@periinc.com>
2016-07-12 12:13:04 -0400

    Added creation-table sql to startup.sh

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -11,7 +11,14 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	echo "Run mysql"
 	/usr/bin/mysqld_safe & sleep 10s
 
-	echo "GRANT ALL ON *.* to etl@'%' IDENTIFIED BY 'etl', recap@'%' IDENTIFIED BY 'recap'; FLUSH PRIVILEGES;" | mysql
+	mysql -uroot < /opt/git/mysql/1_SCHEMA_TABLES_CREATION.sql
+    mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
+    mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
+    mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
+    mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
+    mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
+
+
 
 fi
 	echo "DB already initialized; Starting MySQL"
