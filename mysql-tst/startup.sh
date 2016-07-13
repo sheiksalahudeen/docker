c03b179214349e128cc718327e1ce25cacd04595
premkumarbalu <prem143india>
2016-07-13 15:51:45 +0530

    Configured Dockerfile and my.cnf for mysq-tst

---
+++ mysql-tst/startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+	echo "Copying default my.cnf to /usr/share/ folder"
+	cp /opt/git/mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/mysql-tst/my.cnf /etc/mysql/my.cnf
+		
+	echo "Initialize mysql db"
+	mysql_install_db
+		
+	echo "Run mysql"
+	/usr/bin/mysqld_safe & sleep 10s
+
+	mysql -uroot < /opt/git/mysql/1_SCHEMA_TABLES_CREATION.sql
+    	mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
+    	mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
+    	mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
+    	mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
+    	mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
+
+    	killall mysqld
+
+        sleep 10s
+
+fi
+
+	echo "Copying default my.cnf to /usr/share/ folder"
+	cp /opt/git/mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/mysql-tst/my.cnf /etc/mysql/my.cnf
+
+	echo "DB already initialized; Starting MySQL"
+	/usr/bin/mysqld_safe & tail -f /dev/null
+
