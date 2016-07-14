6c0d18a1a44166eb330a3e5b62ca4bed69886344
premkumarbalu <prem143india>
2016-07-14 12:47:58 +0530

    Updated the appropriate folder location for

--- scsb-mysql-tst/startup.sh
+++ scsb-mysql-tst/startup.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	echo "Copying default my.cnf to /usr/share/ folder"
-	cp /opt/git/mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
-	cp /opt/git/mysql-tst/my.cnf /etc/mysql/my.cnf
+	cp /opt/git/scsb-mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/scsb-mysql-tst/my.cnf /etc/mysql/my.cnf
 		
 	echo "Initialize mysql db"
 	mysql_install_db
@@ -12,11 +12,11 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 	/usr/bin/mysqld_safe & sleep 10s
 
 	mysql -uroot < /opt/git/mysql/1_SCHEMA_TABLES_CREATION.sql
-    	mysql -uroot < /opt/git/mysql/2_COLLECTION_GROUP_T.sql
-    	mysql -uroot < /opt/git/mysql/3_INSTITUTION_T.sql
-    	mysql -uroot < /opt/git/mysql/4_ITEM_STATUS_T.sql
-    	mysql -uroot < /opt/git/mysql/5_REQUEST_TYPE_T.sql
-    	mysql -uroot < /opt/git/mysql/6_ALLOW_CONTAINER_LINKING.sql
+    	mysql -uroot < /opt/git/scsb-mysql-dev/2_COLLECTION_GROUP_T.sql
+    	mysql -uroot < /opt/git/scsb-mysql-dev/3_INSTITUTION_T.sql
+    	mysql -uroot < /opt/git/scsb-mysql-dev/4_ITEM_STATUS_T.sql
+    	mysql -uroot < /opt/git/scsb-mysql-dev/5_REQUEST_TYPE_T.sql
+    	mysql -uroot < /opt/git/scsb-mysql-dev/6_ALLOW_CONTAINER_LINKING.sql
 
     	killall mysqld
 
@@ -25,8 +25,8 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 fi
 
 	echo "Copying default my.cnf to /usr/share/ folder"
-	cp /opt/git/mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
-	cp /opt/git/mysql-tst/my.cnf /etc/mysql/my.cnf
+	cp /opt/git/scsb-mysql-tst/my.cnf /usr/share/mysql/my-default.cnf
+	cp /opt/git/scsb-mysql-tst/my.cnf /etc/mysql/my.cnf
 
 	echo "DB already initialized; Starting MySQL"
 	/usr/bin/mysqld_safe & tail -f /dev/null
