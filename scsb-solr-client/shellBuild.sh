e96c268d4632c259afc01ec28b4389002fc03a65
sudhish74 <sudhish.kutty@htcindia.com>
2016-11-21 17:31:57 +0530

    Fix for git tag using Shell Programming

---
+++ scsb-solr-client/shellBuild.sh
@@ -0,0 +1,28 @@
+
+
+echo "Parameter = $1"
+PROJ_NAME="scsb-solr-client"
+PROJ_DIR="/opt/scsb-solr-client"
+PROJ_DIR_TAG=""
+VAR_SLASH="/"
+
+if [ -z $1 ] ;then	
+	PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
+else
+	PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$1;
+fi
+
+
+# echo $PROJ_DIR_TAG
+mkdir $PROJ_NAME
+cd $PROJ_DIR
+git clone https://github.com/ResearchCollectionsAndPreservation/scsb-solr-client.git $1 
+# echo $PROJ_DIR_TAG
+cd $PROJ_DIR_TAG
+# ls -l
+pwd
+./gradlew clean build -x test
+
+ln -s $PROJ_DIR_TAG/build/libs/scsb-solr-client-0.0.1-SNAPSHOT.jar /etc/init.d/scsb-solr-client
+cp $PROJ_DIR_TAG/build/libs/scsb-solr-client-0.0.1-SNAPSHOT.jar /opt
+
