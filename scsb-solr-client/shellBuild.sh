3444416204180594a1dbed0496626d98544438d8
premkumarbalu <prem143india>
2016-11-28 13:29:59 +0530

    fixed the checkout tag issue

--- scsb-solr-client/shellBuild.sh
+++ scsb-solr-client/shellBuild.sh
@@ -6,19 +6,15 @@ PROJ_DIR="/opt/scsb-solr-client"
 PROJ_DIR_TAG=""
 VAR_SLASH="/"
 
-if [ -z $1 ] ;then	
-	PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
-else
-	PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$1;
-fi
-
+PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
 
 # echo $PROJ_DIR_TAG
 mkdir $PROJ_NAME
 cd $PROJ_DIR
-git clone https://github.com/ResearchCollectionsAndPreservation/scsb-solr-client.git $1 
+git clone https://github.com/ResearchCollectionsAndPreservation/scsb-solr-client.git
 # echo $PROJ_DIR_TAG
 cd $PROJ_DIR_TAG
+git checkout tags/$1
 # ls -l
 pwd
 ./gradlew clean build -x test
