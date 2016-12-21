4f6d69b26dba1cb662a75b989de5c4ea70ca66da
Dharam-heterodox <dharmendra.ganesan@htcindia.com>
2016-12-21 20:19:35 +0530

    Updated the insert script for authentication

--- scsb-mysql/11_PERMISSIONS_T.sql
+++ scsb-mysql/11_PERMISSIONS_T.sql
@@ -1,13 +1,13 @@
 
 
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (1,'CreateUser','To create users,assign roles');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (2,'WriteCGD','To write/edit CGD for own institutions');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (3,'DeAccession','To deaccession records for own institution');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (4,'RequestPlace','Ability to place request within own institution');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (5,'RequestPlaceAll','Ability to place request within all institutions');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (6,'RequestCancel','Ability to cancel request within own institution');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (7,'ViewPrintReports','Ability to view & print reports');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (8,'SCSBSearchExport','Ability to search SCSB and export results');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (9,'BarcodeRestriction','Barcode will be restricted for this role');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (10,'RequestItems','Ability to request items from any institution');
-INSERT INTO PERMISSIONS_T (PERMISSION_ID,PERMISSION_NAME,PERMISSION_DESCRIPTION) VALUES (11,'RequestCancelAll','Ability to cancel any request');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (1,'CreateUser','To create users,assign roles');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (2,'WriteCGD','To write/edit CGD for own institutions');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (3,'DeAccession','To deaccession records for own institution');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (4,'RequestPlace','Ability to place request within own institution');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (5,'RequestPlaceAll','Ability to place request within all institutions');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (6,'RequestCancel','Ability to cancel request within own institution');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (7,'ViewPrintReports','Ability to view & print reports');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (8,'SCSBSearchExport','Ability to search SCSB and export results');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (9,'BarcodeRestriction','Barcode will be restricted for this role');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (10,'RequestItems','Ability to request items from any institution');
+INSERT INTO `recap`.`PERMISSIONS_T` (`PERMISSION_ID`,`PERMISSION_NAME`,`PERMISSION_DESCRIPTION`) VALUES (11,'RequestCancelAll','Ability to cancel any request');
--- scsb-mysql/12_ROLES_T.sql
+++ scsb-mysql/12_ROLES_T.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (1,'SuperAdmin','Admin for all the institutions');
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (2,'Admin','Admin for particular institutions');
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (3,'SearchRequest','Search and Request for particular institution');
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (4,'Collection','Collection user for particular institution');
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (5,'ReCAP','ReCAP Users');
-INSERT INTO ROLES_T (ROLE_ID,ROLE_NAME,ROLE_DESCRIPTION) VALUES (6,'Search','Search users');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (1,'SuperAdmin','Admin for all the institutions');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (2,'Admin','Admin for particular institutions');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (3,'SearchRequest','Search and Request for particular institution');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (4,'Collection','Collection user for particular institution');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (5,'ReCAP','ReCAP Users');
+INSERT INTO `recap`.`ROLES_T` (`ROLE_ID`,`ROLE_NAME`,`ROLE_DESCRIPTION`) VALUES (6,'Search','Search users');
