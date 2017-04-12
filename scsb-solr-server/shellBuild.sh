6a0b359596051f01989aee773b83526d651adf21
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2017-04-11 21:21:07 -0400

    RECAP-647 : Upgraded the Solr version to 6.5.0

--- scsb-solr-server/shellBuild.sh
+++ scsb-solr-server/shellBuild.sh
@@ -1,37 +1,12 @@
-
-
-echo "Parameter = $1"
-PROJ_NAME="scsb-solr"
-PROJ_DIR="/opt/scsb-solr"
-PROJ_DIR_TAG=""
-VAR_SLASH="/"
-
-PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
-
-# echo $PROJ_DIR_TAG
-mkdir $PROJ_NAME
-cd $PROJ_DIR
-git clone https://github.com/ResearchCollectionsAndPreservation/scsb-solr.git
-# echo $PROJ_DIR_TAG
-cd $PROJ_DIR_TAG
-if [ ! -z $1 ] ;then
-    git checkout tags/$1
-fi
-# ls -l
 # Add SCSB-SOLR files
-cd /opt/solr-6.0.1/bin
-./solr start
-./solr create -c recap
-cp $PROJ_DIR_TAG/src/main/resources/core/data-config.xml /opt/solr-6.0.1/server/solr/recap/conf
-cp $PROJ_DIR_TAG/src/main/resources/core/solrconfig.xml /opt/solr-6.0.1/server/solr/recap/conf
-cp $PROJ_DIR_TAG/src/main/resources/core/managed-schema /opt/solr-6.0.1/server/solr/recap/conf
-mkdir /opt/solr-6.0.1/server/solr/configsets/recap_config
+cd /opt/solr-6.5.0/bin
+./solr start -force
+./solr create -c recap -force
+ls /opt/solr-6.5.0/server/solr/
+cp /opt/solr-config/solrconfig.xml /opt/solr-6.5.0/server/solr/recap/conf/solrconfig.xml
+cp /opt/solr-config/managed-schema /opt/solr-6.5.0/server/solr/recap/conf/managed-schema
+mkdir /opt/solr-6.5.0/server/solr/configsets/recap_config
 \
-mkdir /opt/solr-6.0.1/server/solr/configsets/recap_config/conf
+mkdir /opt/solr-6.5.0/server/solr/configsets/recap_config/conf
 \
-cp -R /opt/solr-6.0.1/server/solr/configsets/basic_configs/conf/* /opt/solr-6.0.1/server/solr/configsets/recap_config/conf
-\
-cp $PROJ_DIR_TAG/src/main/resources/core/managed-schema /opt/solr-6.0.1/server/solr/configsets/recap_config/conf
-\
-cp -R /opt/solr-6.0.1/server/solr/recap/conf/lang /opt/solr-6.0.1/server/solr/configsets/recap_config/conf
-cp $PROJ_DIR_TAG/src/main/resources/temp/solrconfig.xml /opt/solr-6.0.1/server/solr/configsets/recap_config/conf
+cp -R /opt/solr-6.5.0/server/solr/recap/conf /opt/solr-6.5.0/server/solr/configsets/recap_config/
