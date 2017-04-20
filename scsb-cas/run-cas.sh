cefdcaec852e4813223ecc118ac515b9f33ddae4
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2017-04-20 09:15:43 -0400

    RECAP-663 : Updated Dockerfile to use tag while deploying code.

--- scsb-cas/run-cas.sh
+++ scsb-cas/run-cas.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
-export JAVA_HOME=/opt/jdk1.8.0_51
-export PATH=$PATH:$JAVA_HOME/bin:.
-# echo "Use of this image/container constitutes acceptence of the Oracle Binary Code License Agreement for Java SE."
-cd /opt/cas-overlay/
-echo -e "Executing build from directory:" && pwd
+echo "Parameter = $1"
+PROJ_NAME="scsb-cas"
+PROJ_DIR="/opt/scsb-cas"
+PROJ_DIR_TAG=""
+VAR_SLASH="/"
+
+PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
+
+cd $PROJ_DIR_TAG
+
+# echo Listing project directory
+ls
+
 exec ./build.sh run
---
+++ scsb-cas/shellBuild.sh
@@ -0,0 +1,44 @@
+
+echo "Parameter = $1"
+PROJ_NAME="scsb-cas"
+PROJ_DIR="/opt/scsb-cas"
+PROJ_DIR_TAG=""
+VAR_SLASH="/"
+
+PROJ_DIR_TAG=$PROJ_DIR$VAR_SLASH$PROJ_NAME;
+
+# echo $PROJ_DIR_TAG
+mkdir $PROJ_NAME
+cd $PROJ_DIR
+
+# echo Cloning poject
+git clone https://github.com/ResearchCollectionsAndPreservation/scsb-cas.git
+
+# echo $PROJ_DIR_TAG
+cd $PROJ_DIR_TAG
+if [ ! -z $1 ] ;then
+    git checkout tags/$1
+fi
+ls -l
+pwd
+
+# echo Preparing config files
+mkdir -p /etc/cas \
+    && mkdir -p /etc/cas/services \
+    && mkdir -p /etc/cas/config \
+    && mkdir -p $PROJ_DIR_TAG/bin \
+    && cp -f $PROJ_DIR_TAG/etc/cas/config/*.* /etc/cas/config;
+
+# echo Copy keystore file
+cp /opt/cas-config/thekeystore /etc/cas/
+
+# echo Listing project directory
+ls $PROJ_DIR_TAG/
+
+# echo Change permissions
+chmod -R 750 $PROJ_DIR_TAG/bin \
+    && chmod 750 $PROJ_DIR_TAG/mvnw \
+    && chmod 750 $PROJ_DIR_TAG/build.sh;
+
+# echo Building application
+./mvnw clean package -T 10
