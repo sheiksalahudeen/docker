7391c69caeb9fa89f3ebeace1ce59f34a3264fda
sudhish74 <sudhish.kutty@htcindia.com>
2017-04-28 12:50:16 +0530

    New Record In Request_item_status_t

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -1,5 +1,5 @@
 USE `recap` ;
 
 -- Script for Release 0.9.22 starts here
-
+INSERT INTO REQUEST_ITEM_STATUS_T (REQUEST_STATUS_ID, REQUEST_STATUS_CODE, REQUEST_STATUS_DESC) VALUES (8,"PROCESSING","PROCESSING ...");
 -- Script for Release 0.9.22 starts here
