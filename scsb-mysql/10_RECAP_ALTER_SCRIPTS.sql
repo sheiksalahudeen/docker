4e5c5b42ece35fdb66b17a4f4d3fe50c24228421
kirankumar03 <akula.kirankumar@htcindia.com>
2017-05-17 16:17:29 +0530

    add script for daily reconcilation in job table

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -2,6 +2,8 @@ USE `recap` ;
 
 -- Script for Release 0.9.23 starts here
 
+INSERT INTO `recap`.`job_t` (`JOB_ID`,`JOB_NAME`,`JOB_DESC`,`LAST_EXECUTED_TIME`,`NEXT_RUN_TIME`,`CRON_EXP`,`STATUS`) VALUES (4,'DailyReconcilation','Generate DailyReconcilation Report',NULL,NULL,NULL,NULL);
+
 DROP TABLE AUDIT_T;
 DROP TABLE LOAN_T;
 
--- scsb-mysql/23_JOB_T.sql
+++ scsb-mysql/23_JOB_T.sql
@@ -5,3 +5,4 @@
 INSERT INTO `recap`.`job_t` (`JOB_ID`,`JOB_NAME`,`JOB_DESC`,`LAST_EXECUTED_TIME`,`NEXT_RUN_TIME`,`CRON_EXP`,`STATUS`) VALUES (1,'PurgeExceptionRequests','Purge Exception Requests',NULL,NULL,NULL,NULL);
 INSERT INTO `recap`.`job_t` (`JOB_ID`,`JOB_NAME`,`JOB_DESC`,`LAST_EXECUTED_TIME`,`NEXT_RUN_TIME`,`CRON_EXP`,`STATUS`) VALUES (2,'PurgeEmailAddress','Purge Email Address',NULL,NULL,NULL,NULL);
 INSERT INTO `recap`.`job_t` (`JOB_ID`,`JOB_NAME`,`JOB_DESC`,`LAST_EXECUTED_TIME`,`NEXT_RUN_TIME`,`CRON_EXP`,`STATUS`) VALUES (3,'MatchingAlgorithm','Matching Algorithm Ongoing',NULL,NULL,NULL,NULL);
+INSERT INTO `recap`.`job_t` (`JOB_ID`,`JOB_NAME`,`JOB_DESC`,`LAST_EXECUTED_TIME`,`NEXT_RUN_TIME`,`CRON_EXP`,`STATUS`) VALUES (4,'DailyReconcilation','Generate DailyReconcilation Report',NULL,NULL,NULL,NULL);
