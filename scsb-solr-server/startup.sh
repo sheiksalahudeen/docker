c193e50e6aefef802429664d195756e23fcc5ee8
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2017-06-02 04:23:16 -0400

    RECAP-768 : Added the option to provide instance host information.

--- scsb-solr-server/startup.sh
+++ scsb-solr-server/startup.sh
@@ -5,7 +5,7 @@
 if [ $REPLICATION = "true" ]
 then
     echo "Starting SOLR container with replication using zookeeper host"
-    /opt/solr-6.5.0/bin/solr start -Dbootstrap_conf=true -z $ZK_HOST_1:2181,$ZK_HOST_2:2181,$ZK_HOST_3:2181 -noprompt -force -f $ENV
+    /opt/solr-6.5.0/bin/solr start -Dbootstrap_conf=true -h $INSTANCE_HOST -z $ZK_HOST_1:2181,$ZK_HOST_2:2181,$ZK_HOST_3:2181 -noprompt -force -f $ENV
 else
     echo "Starting SOLR container with non replication"
     /opt/solr-6.5.0/bin/solr -force -f $ENV
