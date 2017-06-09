7fb6adb1854c3932ad4f3243a72859555944ee22
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2017-06-09 06:18:44 -0400

    RECAP-796 : Provided the option to start ActiveMQ using both MySQL and Kaha Db

--- scsb-activemq/startup.sh
+++ scsb-activemq/startup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-#Replace db url
-sed -i -e 's@replace_your_db_url@'"$DB_URL"'@g' /opt/activemq/conf/activemq.xml
+echo "Start container."
 
-#Replace db username
-sed -i -e 's@replace_your_db_username@'"$DB_USERNAME"'@g' /opt/activemq/conf/activemq.xml
+if [ $DB_PERSISTANCE = "true" ]
+then
+    echo "Starting ActiveMQ with MySQL db Persistance"
+    sed -i -e 's@<!--ReplaceStorageType-->@''<jdbcPersistenceAdapter dataSource="#mysql-ds"/>''@g' /opt/activemq/conf/activemq.xml
 
-#Replace db password
-sed -i -e 's@replace_your_db_password@'"$DB_PASSWORD"'@g' /opt/activemq/conf/activemq.xml
+    echo "Replace db url"
+    sed -i -e 's@replace_your_db_url@'"$DB_URL"'@g' /opt/activemq/conf/activemq.xml
+
+    echo "Replace db username"
+    sed -i -e 's@replace_your_db_username@'"$DB_USERNAME"'@g' /opt/activemq/conf/activemq.xml
+
+    echo "Replace db password"
+    sed -i -e 's@replace_your_db_password@'"$DB_PASSWORD"'@g' /opt/activemq/conf/activemq.xml
+else
+    echo "Starting ActiveMQ with Kaha db Persistance"
+    sed -i -e 's@<!--ReplaceStorageType-->@''<kahaDB directory="${activemq.data}/kahadb"/>''@g' /opt/activemq/conf/activemq.xml
+fi
 
 sed -i -e 's@#--replace_user_infos@'"$USERS"'@g' /opt/activemq/conf/jetty-realm.properties
 
