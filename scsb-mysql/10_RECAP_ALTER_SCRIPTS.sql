bd1b96c43e767751ffe4c387ab3bb10e11766868
Hari-Krishnan19 <krish19nov@gmail.com>
2017-06-13 20:12:20 +0530

    Recap - 797 Added 'View and print reports' permission to 'Search' role

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -2,6 +2,8 @@ USE `recap` ;
 
 -- Script for Release 0.9.26 starts here
 
+INSERT INTO `recap`.`ROLE_PERMISSION_T` (`ROLE_ID`,`PERMISSION_ID`) VALUES (6,7);
+
 -- Script for Release 0.9.26 ends here
 
 
