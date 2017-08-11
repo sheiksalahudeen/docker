3cc303f13df676947651837d636d3e45424b11f6
Hari-Krishnan19 <krish19nov@gmail.com>
2017-08-11 12:11:40 +0530

    MS-2 EDD added for PJ,PT,PW customer codes.

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -1,7 +1,9 @@
 USE `recap` ;
 
 -- Script for Release 0.9.32 starts here
-
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PW';
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PT';
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PJ'
 
 -- Script for Release 0.9.32 ends here
 
