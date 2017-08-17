e9edd0e9a47859fda31635292a6dcc1343bea8c6
Prem Kumar <premkumar.balu@htcindia.com>
2017-08-17 08:23:13 +0530

    Moving alter script to alter script history file after 0.9.32 release

--- scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
+++ scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
@@ -1,5 +1,12 @@
 USE `recap` ;
 
+-- Script for Release 0.9.32 starts here
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PW';
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PT';
+UPDATE `recap`.`customer_code_t` SET `RECAP_DELIVERY_RESTRICTIONS`='EDD' WHERE `CUSTOMER_CODE`='PJ'
+
+-- Script for Release 0.9.32 ends here
+
 -- Script for Release 0.9.30 starts here
 
 ALTER TABLE `recap`.`REPORT_T` MODIFY COLUMN `FILE_NAME` VARCHAR(250);
