d2c49abf72b80f9eed448f4b400046aee02dee40
Hari-Krishnan19 <krish19nov@gmail.com>
2017-09-06 19:51:05 +0530

    MS-70 Changes delivery code for NS

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -1,6 +1,9 @@
 USE `recap` ;
 
 -- Script for Release 0.9.35 starts here
+
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='Schomburg Center MARB', `DELIVERY_RESTRICTIONS`='NS,NC,NK,NX,IN,SA,SM,SP,NY,SR', `PWD_DELIVERY_RESTRICTIONS`='NS,NC,NK,NX,SA,SM,SP,NY,SR' WHERE `CUSTOMER_CODE_ID`='84';
+
 UPDATE `recap`.`customer_code_t` SET `CIRC_DESK_LOCATION`='CIRCrecap' WHERE `CUSTOMER_CODE`='IC';
 UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='401 Butler Library (Microforms Reading Room)' WHERE `CUSTOMER_CODE`='CF';
 -- Script for Release 0.9.35 ends here
