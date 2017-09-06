8303436d05d250e2f63343088243459e097d6db5
kirankumar03 <akula.kirankumar@htcindia.com>
2017-09-06 16:55:26 +0530

    Ms-52 NYPL delivery locations not showing up.

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -1,7 +1,8 @@
 USE `recap` ;
 
 -- Script for Release 0.9.35 starts here
-
+UPDATE `recap`.`customer_code_t` SET `CIRC_DESK_LOCATION`='CIRCrecap' WHERE `CUSTOMER_CODE`='IC';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='401 Butler Library (Microforms Reading Room)' WHERE `CUSTOMER_CODE`='CF';
 -- Script for Release 0.9.35 ends here
 
 
