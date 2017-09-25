6ae79ee0a3bd736f548e86075287211bcf5e85bd
kirankumar03 <akula.kirankumar@htcindia.com>
2017-09-25 16:15:38 +0530

    MS-97 Jobs - Changes to the Job description

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -4,7 +4,7 @@ USE `recap` ;
 
 UPDATE `recap`.`job_t` SET `JOB_DESC`='The job purges the requests that end up with \'Exception\' status and are 365 days old. The number of days is configurable.' WHERE `JOB_NAME`='PurgeExceptionRequests';
 UPDATE `recap`.`job_t` SET `JOB_DESC`='The job purges the patron email addresses entered in SCSB as part of requests. In case of physical requests, it will be purged 90 days after refile and in case of EDD, 60 days from the date of fulfillment. The number of days are configurable.' WHERE `JOB_NAME`='PurgeEmailAddress';
-UPDATE `recap`.`job_t` SET `JOB_DESC`='The job initiates the ongoing matching algorithm process in SCSB. The processes are typically executed daily after the accession and submit collection processes are completed' WHERE `JOB_NAME`='OngoingMatchingAlgorithm';
+UPDATE `recap`.`job_t` SET `JOB_DESC`='The job initiates the ongoing matching algorithm process in SCSB. The processes are typically executed daily after the accession and submit collection processes are completed.' WHERE `JOB_NAME`='OngoingMatchingAlgorithm';
 UPDATE `recap`.`job_t` SET `JOB_DESC`='The job reconciles the daily LAS transactions with SCSB.' WHERE `JOB_NAME`='DailyLASTransactionReconciliation';
 UPDATE `recap`.`job_t` SET `JOB_DESC`='The job generates an ongoing accession report.' WHERE `JOB_NAME`='GenerateAccessionReport';
 UPDATE `recap`.`job_t` SET `JOB_DESC`='The job initiates the batch ongoing accession process where barcodes that were accumulated throughout the day are processed.' WHERE `JOB_NAME`='Accession';
